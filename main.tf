e603182f899d128c51ae66881713bb2e299de5d1
Nikhil <nikhilvadla500@gmail.com>
2025-11-09 21:00:17 +0530

    added here

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my_bucket" {
   name                     = "github-terraform-bucket1"
-  project                  =  "github-demo-bucket"
+  project                  = "github-demo-bucket"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
