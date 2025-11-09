a352aeba325d529d6e77dce48fb87b3eec3f7975
Nikhil <nikhilvadla500@gmail.com>
2025-11-09 19:28:27 +0530

    commited here

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "google_storage_bucket" "my_bucket" {
   name                     = "github-terraform-bucket1"
   location                 = "US"
   force_destroy            = true
-  public_access_prevention = "enforce"
+  public_access_prevention = "enforced"
 }
