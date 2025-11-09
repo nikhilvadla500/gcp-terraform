e07cccf06d2adf81c97fa378c733341c6860d7b2
Nikhil <nikhilvadla500@gmail.com>
2025-11-09 21:02:42 +0530

    final modified

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my_bucket" {
   name                     = "github-terraform-bucket1"
-  project                  = "github-demo-bucket"
+  project                  = "careful-ensign-470212-v5"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
